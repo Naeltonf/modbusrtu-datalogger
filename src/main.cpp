b6d3db878acbc7e92bdfe27e1b8b9b4a19d92cbd
Naeltonf <naeltonfdasilva@gmail.com>
2024-08-15 16:04:52 -0300

    Updated V1 FW

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,14 @@
 #include <SoftwareSerial.h>
 
 #define SLAVE_ID 1
-//numero inicial
-//#define FIRST_REG 15
 #define FIRST_REG 107
 #define REG_COUNT 6
 #define D1 26
 #define D2 25
+#define PIN_SERIAL 17
+#define PIN_MODBUS 16
+
+String var;
 
 int primeiroByteDados;
 int segundoByteDados;
@@ -20,20 +22,16 @@ SoftwareSerial serialTransparente(19, 18); // RX - TX
 SoftwareSerial S(D1, D2);
 ModbusRTU mb;
 
-bool cb(Modbus::ResultCode event, uint16_t transactionId, void *data)
-{ // Callback to monitor errors
-  if (event != Modbus::EX_SUCCESS)
-  {
+bool cb(Modbus::ResultCode event, uint16_t transactionId, void *data) { 
+  if (event != Modbus::EX_SUCCESS) {
     Serial.print("Request result: 0x");
     Serial.print(event, HEX);
   }
   return true;
 }
 
-float integer_to_float(uint16_t reg1, uint16_t reg2)
-{
-  union converter
-  {
+float integer_to_float(uint16_t reg1, uint16_t reg2) {
+  union converter {
     float f;
     uint16_t i[2];
   };
@@ -45,208 +43,85 @@ float integer_to_float(uint16_t reg1, uint16_t reg2)
   return f_number.f;
 }
 
-void setup()
-{
+void setup() {
   pinMode(12, OUTPUT);
   pinMode(14, OUTPUT);
   pinMode(27, OUTPUT);
+
+  pinMode(PIN_SERIAL, INPUT);
+  pinMode(PIN_MODBUS, INPUT);
 
   Serial.begin(9600);
   S.begin(9600, SWSERIAL_8N1);
   serialTransparente.begin(9600);
   mb.begin(&S);
   mb.master();
-  Serial.print("Terminou o setup");
+  Serial.println("Terminou o setup");
 }
 
-void loop()
-{
-  digitalWrite(12, HIGH);
-  delay(250);
-  digitalWrite(12, LOW);
-  delay(250);
-
-  digitalWrite(14, HIGH);
-  delay(250);
-  digitalWrite(14, LOW);
-  delay(250);
-
+void loop() {
   digitalWrite(27, HIGH);
 
-  serialTransparente.println("teste");
-  delay(2000);
-
-  uint16_t res[REG_COUNT];
-  float f_res1, f_res2, f_res3;
-
-  if (!mb.slave())
-  {
-    mb.readIreg(SLAVE_ID, FIRST_REG, res, REG_COUNT, cb);
-    while (mb.slave())
-    {
-      mb.task();
-      delay(10);
+  if (digitalRead(PIN_SERIAL) == HIGH && digitalRead(PIN_MODBUS) == LOW) {
+    if (S.available() > 0) {
+      var = S.read();  // Modificado para ler a String completa
+      Serial.print("Value: ");
+      Serial.println(var);
+      serialTransparente.println(var);
     }
+    delay(100);
+  } else {
+    uint16_t res[REG_COUNT];
+    float f_res1, f_res2, f_res3;
 
-    Serial.println();
-    Serial.print("Primeiro Byte: ");
-    primeiroByteDados = res[0];
-    Serial.println(primeiroByteDados);
+    if (!mb.slave()) {
+      mb.readIreg(SLAVE_ID, FIRST_REG, res, REG_COUNT, cb);
+      while (mb.slave()) {
+        mb.task();
+        delay(10);
+      }
 
-    Serial.print("Segundo Byte: ");
-    segundoByteDados = res[1];
-    Serial.println(segundoByteDados);
+      Serial.println();
+      Serial.print("Primeiro Byte: ");
+      primeiroByteDados = res[0];
+      Serial.println(primeiroByteDados);
 
-    Serial.print("Terceiro Byte: ");
-    terceiroByteDados = res[2];
-    Serial.println(terceiroByteDados);
+      Serial.print("Segundo Byte: ");
+      segundoByteDados = res[1];
+      Serial.println(segundoByteDados);
 
-    Serial.print("Quarto Byte: ");
-    quartoByteDados = res[3];
-    Serial.println(quartoByteDados);
+      Serial.print("Terceiro Byte: ");
+      terceiroByteDados = res[2];
+      Serial.println(terceiroByteDados);
 
-    Serial.print("Quinto Byte: ");
-    quintoByteDados = res[4];
-    Serial.println(quintoByteDados);
+      Serial.print("Quarto Byte: ");
+      quartoByteDados = res[3];
+      Serial.println(quartoByteDados);
 
-    Serial.print("Sexto Byte: ");
-    sextoByteDados = res[5];
-    Serial.println(sextoByteDados);
+      Serial.print("Quinto Byte: ");
+      quintoByteDados = res[4];
+      Serial.println(quintoByteDados);
 
-    // Convert each pair of registers to float
-    f_res1 = integer_to_float(res[0], res[1]);
-    f_res2 = integer_to_float(res[2], res[3]);
-    f_res3 = integer_to_float(res[4], res[5]);
+      Serial.print("Sexto Byte: ");
+      sextoByteDados = res[5];
+      Serial.println(sextoByteDados);
 
-    //Serial.println("Float 1: ");
-    //Serial.println(f_res1, 3);
+      f_res1 = integer_to_float(res[0], res[1]);
+      f_res2 = integer_to_float(res[2], res[3]);
+      f_res3 = integer_to_float(res[4], res[5]);
 
-    //Serial.println("Float 2: ");
-    //Serial.println(f_res2, 3);
-
-    //Serial.println("Float 3: ");
-    //Serial.println(f_res3, 3);
-
-    serialTransparente.println(primeiroByteDados);
-    delay(2000);
-    serialTransparente.println(segundoByteDados);
-    delay(2000);
-    serialTransparente.println(terceiroByteDados);
-    delay(2000);
-    serialTransparente.println(quartoByteDados);
-    delay(2000);
-    serialTransparente.println(quintoByteDados);
-    delay(2000);
-    serialTransparente.println(sextoByteDados);
-    delay(2000);
-  }
-}
-
-/*
-
-#include <ModbusRTU.h>
-#include <SoftwareSerial.h>
-
-#define SLAVE_ID 1
-#define FIRST_REG 15
-#define REG_COUNT 2
-#define D1 26
-#define D2 25
-
-int primeiroByteDados;
-int segundoByteDados;
-
-SoftwareSerial serialTransparente(18, 19); // RX - TX
-SoftwareSerial S(D1, D2);
-ModbusRTU mb;
-
-bool cb(Modbus::ResultCode event, uint16_t transactionId, void *data)
-{ // Callback to monitor errors
-  if (event != Modbus::EX_SUCCESS)
-  {
-    Serial.print("Request result: 0x");
-    Serial.print(event, HEX);
-  }
-  return true;
-}
-
-float integer_to_float(uint16_t reg1, uint16_t reg2)
-{
-  union converter
-  {
-    float f;
-    uint16_t i[2];
-  };
-
-  union converter f_number;
-  f_number.i[0] = reg2;
-  f_number.i[1] = reg1;
-
-  return f_number.f;
-}
-
-void setup()
-{
-  pinMode(12, OUTPUT);
-  pinMode(14, OUTPUT);
-  pinMode(27, OUTPUT);
-
-  Serial.begin(9600);
-  S.begin(9600, SWSERIAL_8N1);
-  serialTransparente.begin(9600);
-  mb.begin(&S);
-  mb.master();
-  Serial.print("Terminou o setup");
-}
-
-void loop()
-{
-  digitalWrite(12, HIGH);
-  delay(250);
-  digitalWrite(12, LOW);
-  delay(250);
-
-  digitalWrite(14, HIGH);
-  delay(250);
-  digitalWrite(14, LOW);
-  delay(250);
-
-  digitalWrite(27, HIGH);
-
-  serialTransparente.println("teste");
-  delay(2000);
-
-  uint16_t res[REG_COUNT];
-  float f_res;
-
-  if (!mb.slave())
-  {                                                       // Check if no transaction in progress
-    mb.readIreg(SLAVE_ID, FIRST_REG, res, REG_COUNT, cb); // Send Read Hreg from Modbus Server
-    while (mb.slave())
-    { // Check if transaction is active
-      mb.task();
-      delay(10);
+      serialTransparente.println(primeiroByteDados);
+      delay(2000);
+      serialTransparente.println(segundoByteDados);
+      delay(2000);
+      serialTransparente.println(terceiroByteDados);
+      delay(2000);
+      serialTransparente.println(quartoByteDados);
+      delay(2000);
+      serialTransparente.println(quintoByteDados);
+      delay(2000);
+      serialTransparente.println(sextoByteDados);
+      delay(2000);
     }
-    Serial.println();
-    Serial.print("Primeiro Byte: ");
-    // Serial.println(res[0], HEX);
-    primeiroByteDados = res[0];
-    Serial.println(primeiroByteDados);
-
-    Serial.print("Segundo Byte: ");
-    // Serial.println(res[1], HEX);
-    segundoByteDados = res[1];
-    Serial.println(segundoByteDados);
-
-    f_res = integer_to_float(res[0], res[1]);
-    Serial.println("Float: ");
-    Serial.println(f_res, 3);
-
-    // serialTransparente.println(primeiroByteDados);
-    delay(7000);
-    // serialTransparente.println("teste");
   }
-  delay(7000);
 }
-
-*/
