faa6ba5579e88ab7fdeda7fcbf94e68a6ab452ef
Naeltonf <naeltonfdasilva@gmail.com>
2024-08-16 10:08:43 -0300

    Updeted Perf

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,9 @@
 #define D2 25
 #define PIN_SERIAL 17
 #define PIN_MODBUS 16
+#define LED_ESP 27
+#define LED_SERIAL 12
+#define LED_RF 14
 
 String var;
 int dataBuffer[13];
@@ -51,9 +54,9 @@ float integer_to_float(uint16_t reg1, uint16_t reg2)
 
 void setup()
 {
-  pinMode(27, OUTPUT); // ONOFF
-  pinMode(12, OUTPUT); // COMUNICAÇÃO
-  pinMode(14, OUTPUT); // RF
+  pinMode(LED_ESP, OUTPUT);    // LED para indicar que o ESP32 está ligado
+  pinMode(LED_SERIAL, OUTPUT); // LED para indicar comunicação via Serial
+  pinMode(LED_RF, OUTPUT);     // LED para indicar comunicação via RF
 
   pinMode(PIN_SERIAL, INPUT);
   pinMode(PIN_MODBUS, INPUT);
@@ -63,49 +66,57 @@ void setup()
   serialTransparente.begin(9600);
   mb.begin(&S);
   mb.master();
+
+  // Acende o LED ao iniciar o ESP32
+  digitalWrite(LED_ESP, HIGH);
   Serial.println("Terminou o setup");
 }
 
 void loop()
 {
-  digitalWrite(27, HIGH);
-
   if (digitalRead(PIN_SERIAL) == HIGH && digitalRead(PIN_MODBUS) == LOW)
   {
-
     if (S.available() > 0)
     {
       int receivedValue = S.read(); // Lê o valor recebido
-      digitalWrite(12, HIGH);
-      delay(250);
-      digitalWrite(12, LOW);
+      Serial.println("Dado recebido na Serial.");
+
+      // Pisca o LED para indicar que recebeu dado na Serial
+      digitalWrite(LED_SERIAL, HIGH);
+      delay(300);
+      digitalWrite(LED_SERIAL, LOW);
+
       dataBuffer[bufferIndex] = receivedValue; // Armazena no buffer
       bufferIndex++;                           // Incrementa o índice do buffer
 
-      // Se o buffer estiver cheio (13 leituras), envia os dados e reseta o buffer
-      if (bufferIndex >= 13)
+      // Se o buffer estiver cheio (12 leituras), envia os dados e reseta o buffer
+      if (bufferIndex >= 12)
       {
-        for (int i = 0; i < 13; i++)
+        for (int i = 0; i < 12; i++)
         {
           serialTransparente.println(dataBuffer[i]);
-          Serial.println(dataBuffer[i]);
+          Serial.println("Dado enviado via RF: " + String(dataBuffer[i]));
         }
+
+        // Pisca o LED para indicar que houve envio de dados via RF
+        digitalWrite(LED_RF, HIGH);
+        delay(300);
+        digitalWrite(LED_RF, LOW);
 
         // Reseta o buffer e o índice
         bufferIndex = 0;
       }
     }
-    /* if (S.available() > 0) {
-       var = S.read();  // Modificado para ler a String completa
-       Serial.print("Value: ");
-       Serial.println(var);
-       serialTransparente.println(var);
-     }
-     delay(100);
-   */
   }
   else
   {
+    Serial.println("Dado recebido na Serial.");
+
+    // Pisca o LED para indicar que recebeu dado na Serial
+    digitalWrite(LED_SERIAL, HIGH);
+    delay(300);
+    digitalWrite(LED_SERIAL, LOW);
+
     uint16_t res[REG_COUNT];
     float f_res1, f_res2, f_res3;
 
@@ -121,12 +132,10 @@ void loop()
       // Criação de um buffer String para armazenar todos os dados
       String dataBuffer = "";
 
-      dataBuffer += "Primeiro Byte: " + String(res[0]) + "\n";
-      dataBuffer += "Segundo Byte: " + String(res[1]) + "\n";
-      dataBuffer += "Terceiro Byte: " + String(res[2]) + "\n";
-      dataBuffer += "Quarto Byte: " + String(res[3]) + "\n";
-      dataBuffer += "Quinto Byte: " + String(res[4]) + "\n";
-      dataBuffer += "Sexto Byte: " + String(res[5]) + "\n";
+      dataBuffer += "AL 09: " + String(res[0]) + "\n";
+      dataBuffer += "AL 10: " + String(res[1]) + "\n";
+      dataBuffer += "AL 11: " + String(res[2]) + "\n";
+      dataBuffer += "AL 12: " + String(res[3]) + "\n";
 
       f_res1 = integer_to_float(res[0], res[1]);
       f_res2 = integer_to_float(res[2], res[3]);
@@ -134,6 +143,12 @@ void loop()
 
       // Enviar todos os dados de uma só vez
       serialTransparente.print(dataBuffer);
+      Serial.println("Dados enviados via RF:");
+
+      // Pisca o LED para indicar que houve envio de dados via RF
+      digitalWrite(LED_RF, HIGH);
+      delay(300);
+      digitalWrite(LED_RF, LOW);
 
       // Limpar o buffer após enviar os dados
       dataBuffer = "";
